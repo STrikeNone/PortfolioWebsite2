1a5232396f8748dfa876f3900d1f6b61de0db701
STrikeNone <yihao.puah@gmail.com>
2025-05-18 15:54:20 +0800

    Update to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"; // ✅ Use HashRouter
 import Navbar from "./components/NavBar";
 import About from "./pages/About";
 import Resume from "./pages/Resume";
